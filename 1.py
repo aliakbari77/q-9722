eba5c5715da4ca7994fa9bb89a3daba0a610556b
Ali Akbari <7731.aa@gmail.com>
2022-10-19 10:27:45 +0330

    feat: solve :)

---
+++ 1.py
@@ -0,0 +1,30 @@
+import math
+
+
+def checkPrime(n):
+    if (n == 1):
+        return False
+    
+    maxCheck = int(n/2) + 1
+    for i in range(2, maxCheck):
+        if (n%i == 0):
+            return False
+    return True
+    
+digitsNumber = int(input())
+
+minimum = int(math.pow(10, digitsNumber - 1))
+maximum = int(math.pow(10, digitsNumber))
+
+for i in range(minimum, maximum + 1):
+    n = i
+    t = False
+    while n:
+        t = checkPrime(n)
+        if (t):
+            n = int(n/10)
+        else:
+            break
+    if (t):
+        print(i)
+
